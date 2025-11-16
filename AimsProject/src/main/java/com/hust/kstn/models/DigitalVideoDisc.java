086b637bf0c803397c5b3500cca9c5e53b4cdf34
zangdo <zangdo20052014@gmail.com>
2025-11-16 11:54:01 +0700

    up lan 6

--- AimsProject/src/main/java/com/hust/kstn/models/DigitalVideoDisc.java
+++ AimsProject/src/main/java/com/hust/kstn/models/DigitalVideoDisc.java
@@ -1,27 +1,32 @@
 package com.hust.kstn.models;
 
 public class DigitalVideoDisc {
+    private int id;
+    private static int nbDigitalVideoDiscs = 0;
     private String title;
     private String genre;
     private double price;
     private String director;
     private int length;
 
+
     public DigitalVideoDisc(String title) {
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
         this.title = title;
     }
     public DigitalVideoDisc(String title, String genre, double price, String director) {
-        this.title = title;
+        this(title);
         this.genre = genre;
         this.price = price;
         this.director = director;
     }
     public DigitalVideoDisc(String title, String genre, double price, String director, int length) {
-        this.title = title;
-        this.genre = genre;
-        this.price = price;
-        this.director = director;
+        this(title, genre, price, director);
         this.length = length;
+    }
+    public int getId() {
+        return id;
     }
     public String getTitle() {
         return title;
