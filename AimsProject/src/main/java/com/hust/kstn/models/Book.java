640217b201c3ab61bb9e651dea0d4596fc080019
zangdo <zangdo20052014@gmail.com>
2025-11-18 21:45:02 +0700

    up lan 11

--- AimsProject/src/main/java/com/hust/kstn/models/Book.java
+++ AimsProject/src/main/java/com/hust/kstn/models/Book.java
@@ -1,39 +1,16 @@
 package com.hust.kstn.models;
 import java.util.ArrayList;
 import java.util.List;
-public class Book {
-    private int id;
-    private static int nbBooks = 0;
-    private String title;  
-    private String genre;
-    private double price;
+public class Book extends Media {
     private List<BookAuthor> authors = new ArrayList<>();
     private int length; // Số tuwf
-    public Book(String title) {
-        nbBooks++;
-        this.id = nbBooks;
-        this.title = title;
-    }
     public Book(String title, String genre, double price, int length, BookAuthor... authors) {
-        this(title);
-        this.genre = genre;
-        this.price = price;
+        super(title, genre, price);
         this.length = length;
+        //ko neen gan luon vi se bi trung dia chi o ngoai
         for (BookAuthor author : authors) {
             this.authors.add(author);
         }
-    }
-    public int getId() {
-        return id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public String getGenre() {
-        return genre;
-    }
-    public double getPrice() {
-        return price;
     }
     public List<BookAuthor> getAuthors() {
         return authors;
@@ -47,7 +24,7 @@ public class Book {
         for (BookAuthor author : this.authors) {
             authorsString.append(author.toString());
         }
-        return String.format("Book[%d] - [%s] - [%s] - [%d pages] - [%.2f$]%nAuthors:%n%s",
-                this.id, this.title, this.genre, this.length, this.price, authorsString.toString());
+        return String.format("Book %s - [%d words]%nAuthors:%n%s",
+                super.toString(), this.length, authorsString.toString());
     }
 }
